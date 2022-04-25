c14277c87d46b182f70faf258af630a19cb5beeb
Somrage <somrage@gmail.com>
2022-04-25 16:30:36 +0700

    Beta version of the game is working!

--- script.js
+++ script.js
@@ -22,15 +22,35 @@ function round(playerChoice, computerChoice) {
     else if ((playerChoice === "rock" && computerChoice === "scissors") || (playerChoice === "paper" && computerChoice === "rock") || (playerChoice === "scissors" && computerChoice === "paper")) {
         result = `You win! ${playerChoice} beats ${computerChoice}`;
         console.log(result);
+        playerScore = +1
     }
     else {
         result = `You lose this round! ${computerChoice} beats ${playerChoice}`;
         console.log(result);
+        computerScore = +1
     }
+
+    return playerScore, computerScore;
    
 }
 
-let playerChoice = prompt("Your choice?", "")
-playerChoice = playerChoice.toLowerCase()
+function game() {
+    for (i = 1 ; i <= 5 ; i++) {
+        let playerChoice = prompt("Enter \"rock\", \"paper\" or \"scissors\"", "");
+        playerChoice = playerChoice.toLowerCase();
+        
+        if (playerChoice != "rock" || playerChoice != "paper" || playerChoice != "scissors") {
+            alert(`${playerChoice} is't allowed! Restart the game`)
+            break
+        }
 
-round(playerChoice, computerPlay());
+        round(playerChoice, computerPlay());
+        let gameScore = `Your score: ${playerScore} VS Computer score: ${computerScore}`;
+        console.log(gameScore);
+    }
+}
+
+let playerScore = 0;
+let computerScore = 0;
+
+game();
