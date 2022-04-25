77a12d370effeab5a4255ffd8581913351bf8833
Somrage <somrage@gmail.com>
2022-04-25 15:06:26 +0700

    Add function computerChoice()

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("hello!")
+let playerChoice = prompt("Your choice?", "")
+playerChoice = playerChoice.toLowerCase()
+
+function computerPlay() {
+    let computerChoice = Math.floor(Math.random()*3) + 1;
+    if (computerChoice == 1) {
+        computerChoice = "rock"
+    }
+    else if (computerChoice == 2) {
+        computerChoice = "paper"
+    }
+    else {
+        computerChoice = "scissors"
+    }
+
+    return computerChoice
+}
